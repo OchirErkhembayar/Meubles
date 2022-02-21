dd5b86a85892f968ad5116acd89b17a3776c0981
Deniz Kazdal <denizkazdal99@gmail.com>
2022-02-21 17:42:53 +0000

    Index and Show Actions for Offers Controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,11 @@
 class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+
+  def show
+    @offer = Offer.find(params[:id])
+    @offer = Offer.new
+  end
 end
