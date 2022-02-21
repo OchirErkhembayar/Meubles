30fc140970480e85a89f9bdc6bb0c08b940a9a7d
Vivek Iyer <vivek.k.iyer@gmail.com>
2022-02-21 21:59:25 +0530

    validations improved

--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -4,6 +4,6 @@ class Furniture < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
   validates :price, presence: true
   validates :location, presence: true, inclusion: {
-    in: "London Edinburgh Cambridge Glasgow Liverpool Birmingham Manchester"
+    in: ["London", "Edinburgh", "Cambridge", "Glasgow", "Liverpool", "Birmingham", "Manchester"]
   }
 end
