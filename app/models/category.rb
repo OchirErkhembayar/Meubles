0c065a7229a688bfc0a9d2828218122dacf766d8
Vivek Iyer <vivek.k.iyer@gmail.com>
2022-02-21 21:43:45 +0530

    validations completed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :furnitures
+  validates :name, presence: true, length: { minimum: 3 }
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :furniture
   belongs_to :user
+  validates :start_date, presence: true, in_future: true
+  validates :end_date, presence: true, in_future: true
 end
