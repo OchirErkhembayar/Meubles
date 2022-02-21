6366e96e9eef338ce58957b5c72d20bdc880b371
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-02-21 23:35:22 +0000

    made changes to both controllers, views, added index and new and show methods for them and small change to validations of furniture and category model.

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -1,2 +1,27 @@
 class FurnituresController < ApplicationController
+
+  def index
+    @furnitures = Furniture.all
+  end
+
+  def show
+    @furniture = Furniture.find(params[:id])
+  end
+
+  def new
+    @furniture = Furniture.new
+  end
+
+  def create
+    @furniture = Furniture.new(set_furniture)
+    @furniture.user_id = current_user.id
+    @furniture.save!
+    redirect_to 'furnitures'
+  end
+
+  private
+
+  def set_furniture
+    params.require(:furniture).permit(:name, :price, :location, :category_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.first
+chair = Furniture.new(name: "chair", price: 5, location: "London")
+puts user.furnitures.first
+puts chair
+
+# puts "creating category: 'old'"
+# old = Category.create!(name: "old")
+# puts "old category created"
