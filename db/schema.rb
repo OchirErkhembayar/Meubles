3bcbb17d59157dbeb0d8a31ee28918f82a58ce3c
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-02-22 14:55:21 +0000

    description added to furniture and added to new furniture form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_115519) do
+ActiveRecord::Schema.define(version: 2022_02_22_144725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_115519) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["category_id"], name: "index_furnitures_on_category_id"
     t.index ["user_id"], name: "index_furnitures_on_user_id"
   end
