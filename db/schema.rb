5f7fdf1fcb0879a776f2caa1d5d8de0aeefe137e
Henry Monkhouse <henrymonkhouse@hotmail.com>
2022-02-24 11:28:58 +0000

    Big Pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_085509) do
+ActiveRecord::Schema.define(version: 2022_02_24_112314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2022_02_24_085509) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
-    t.boolean "rented"
+    t.boolean "rented", default: false
     t.index ["category_id"], name: "index_furnitures_on_category_id"
     t.index ["user_id"], name: "index_furnitures_on_user_id"
   end
